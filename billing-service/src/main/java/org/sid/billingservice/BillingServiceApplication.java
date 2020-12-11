a98ec55368363d8cc572da0097dd8a85f315564a
Medomane <mohamed-hamman@outlook.com>
2020-12-11 21:12:58 +0100

    tests

--- billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
@@ -1,13 +1,15 @@
 package org.sid.billingservice;
 
+import org.sid.billingservice.Feign.CustomerRestClient;
+import org.sid.billingservice.Model.Customer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
-@EnableFeignClients
 @SpringBootApplication
+@EnableFeignClients
 public class BillingServiceApplication{
 
     public static void main(String[] args) {
@@ -15,9 +17,10 @@ public class BillingServiceApplication{
     }
 
     @Bean
-    CommandLineRunner start(){
+    CommandLineRunner start(CustomerRestClient customerRestClient){
         return args ->{
-            System.out.println("salam");
+            Customer c = customerRestClient.getCustomerById(1L);
+            System.out.println(c.toString());
         };
     }
 }
