482cb134f5db959ad2a133a910f6b69b26e8632e
Medomane <mohamed-hamman@outlook.com>
2020-12-12 21:40:07 +0100

    Security service

--- billing-service/src/main/java/org/sid/billingservice/Controller/BillController.java
+++ billing-service/src/main/java/org/sid/billingservice/Controller/BillController.java
@@ -4,7 +4,6 @@ import org.sid.billingservice.Feign.CustomerRestClient;
 import org.sid.billingservice.Feign.ProductRestClient;
 import org.sid.billingservice.Model.Bill;
 import org.sid.billingservice.Repository.BillRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
